f8db34911c4c38b9821355d6fc210d0ffd8fa8ed
Hervé Pagès <hpages@fredhutch.org>
2019-08-08 10:10:44 -0700

    Fix PKG_LIBS on Mac
    Also greatly simplify Rhtslib::pkgconfig().

--- R/zzz.R
+++ R/zzz.R
@@ -7,45 +7,14 @@ pkgconfig <- function(opt=c("PKG_LIBS", "PKG_CPPFLAGS"))
             system.file("usrlib", package="Rhtslib", mustWork=TRUE)
         )
         platform <- Sys.info()[["sysname"]]
-        if (platform == "Windows") {
-            usrlib_dir <- sprintf("%s/%s", usrlib_dir, .Platform[["r_arch"]])
-            ## See how 'htslib_default_libs' is defined in
-            ## htslib-1.7/Makefile.Rhtslib.win and make sure
-            ## to use the same value here.
-            htslib_default_libs <- "-lz -lm -lws2_32 -lcurl"
-            config <- sprintf('-L"%s" -lhts %s -lpthread',
-                              usrlib_dir, htslib_default_libs)
-        } else {
-            if (platform == "Darwin") {
-                config <- sprintf('%s/libhts.a', usrlib_dir)
-            } else {
-                ## See how 'htslib_default_libs' is defined in
-                ## htslib-1.7/Makefile.Rhtslib and make sure
-                ## to use the same value here.
-                htslib_default_libs <- "-lz -lm -lbz2 -llzma -lcurl"
-
-                ## Dynamic linking seems to cause problems on some Linux
-                ## systems. See https://support.bioconductor.org/p/120529/
-                ## Mike's fix is to use linker option --disable-new-dtags:
-                ##   https://github.com/Bioconductor/Rhtslib/pull/5
-                ## Another way to go is to just link statically.
-
-                ## Cause problems on some Linux systems:
-                #config <- sprintf('-L%s -Wl,-rpath,%s -lhts %s -lpthread',
-                #                  usrlib_dir, usrlib_dir, htslib_default_libs)
-
-                ## Using linker option --disable-new-dtags seems to solve
-                ## the problem:
-                #config <- sprintf('-L%s %s -Wl,-rpath,%s -lhts %s -lpthread',
-                #                  usrlib_dir, "-Wl,--disable-new-dtags",
-                #                  usrlib_dir, htslib_default_libs)
-
-                ## However, static linking is probably safer:
-                config <- sprintf('%s/libhts.a %s -lpthread',
-                                  usrlib_dir, htslib_default_libs)
-            }
-        }
+        if (platform == "Windows")
+            usrlib_dir <- file.path(usrlib_dir, .Platform[["r_arch"]])
+        ## See how PKG_LIBS is defined in Rhtslib/src/Makevars.common
+        ## and make sure to produce the same value here.
+        config <- sprintf('%s -lcurl', file.path(usrlib_dir, "libhts.a"))
     } else {
+        ## See how PKG_CPPFLAGS is defined in Rhtslib/src/Makevars.common
+        ## and make sure to produce the same value here.
         include_dir <- system.file("include", package="Rhtslib")
         config <- sprintf('-I"%s"', include_dir)
     }
