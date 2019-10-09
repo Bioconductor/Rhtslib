f412056956890d169f6ca08265cf62d7013d9463
Hervé Pagès <hpages@fredhutch.org>
2019-10-09 11:34:57 -0700

    Update the doc to reflect some of the recent changes

--- R/zzz.R
+++ R/zzz.R
@@ -29,8 +29,12 @@ pkgconfig <- function(opt=c("PKG_LIBS", "PKG_CPPFLAGS"))
     } else {
         ## See how PKG_CPPFLAGS is defined in Rhtslib/src/Makevars.common
         ## and make sure to produce the same value here.
-        include_dir <- system.file("include", package="Rhtslib")
-        config <- sprintf('-D_FILE_OFFSET_BITS=64 -I"%s"', include_dir)
+        config <- "-D_FILE_OFFSET_BITS=64"
+        ## Packages that link to Rhtslib should have Rhtslib in their
+        ## LinkingTo field so the preprocessor option below will automatically
+        ## be added. There is no need to add it again here.
+        #include_dir <- system.file("include", package="Rhtslib")
+        #config <- paste(config, sprintf('-I"%s"', include_dir))
     }
     cat(config)
 }
